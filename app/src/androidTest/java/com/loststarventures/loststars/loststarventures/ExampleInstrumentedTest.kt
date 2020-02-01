62402756956c072c265b7ce9e33794503c48a25e
DeepayanDas <ayan200613@gmail.com>
2020-02-01 23:40:45 +0100

    Initial commit

--- app/src/androidTest/java/com/loststarventures/loststars/loststarventures/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/loststarventures/loststars/loststarventures/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.loststarventures.loststars.loststarventures", appContext.packageName)
+        assertEquals("com.com.loststarventures.loststars.com.loststarventures", appContext.packageName)
     }
 }
